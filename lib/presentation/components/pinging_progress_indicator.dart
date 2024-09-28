369896f17cee37c4264ee3414f579b9153297d26
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-09-29 00:15:04 +0500

    Update pinging_progress_indicator.dart

--- lib/presentation/components/pinging_progress_indicator.dart
+++ lib/presentation/components/pinging_progress_indicator.dart
@@ -3,9 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinging/logic/blocs/app_bloc/app_bloc.dart';
 
 class PingingProgressIndicator extends StatelessWidget {
-  const PingingProgressIndicator({
-    Key? key,
-  }) : super(key: key);
+  const PingingProgressIndicator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,27 +17,35 @@ class PingingProgressIndicator extends StatelessWidget {
         if (state is AppStateInitialPingingProgress) count = state.chunkCount;
 
         return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(
             count,
             (index) => Expanded(
-              child: BlocBuilder<AppBloc, AppState>(
-                buildWhen: (_, state) =>
-                    state is AppStatePingingProgress && state.process == index,
-                builder: (context, state) {
-                  if (state is! AppStatePingingProgress) return nothing;
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: BlocBuilder<AppBloc, AppState>(
+                  buildWhen: (_, state) =>
+                      state is AppStatePingingProgress && state.process == index,
+                  builder: (context, state) {
+                    if (state is! AppStatePingingProgress) return nothing;
 
-                  final progress = state.progress;
+                    final progress = state.progress;
 
-                  if (progress.value == 0 || progress.value == 1) {
-                    return nothing;
-                  }
+                    if (progress.value == 0 || progress.value == 1) {
+                      return nothing;
+                    }
 
-                  return LinearProgressIndicator(
-                    color: Colors.green,
-                    minHeight: 10,
-                    value: progress.value,
-                  );
-                },
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: LinearProgressIndicator(
+                        value: progress.value,
+                        color: Colors.greenAccent,
+                        backgroundColor: Colors.grey.shade300,
+                        minHeight: 12,
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ),
