050911f5739bcb4d64e8970e64ec6b08d44cd46c
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-09-29 00:17:44 +0500

    Update sstp_address_card.dart

--- lib/presentation/components/cards/sstp_address_card.dart
+++ lib/presentation/components/cards/sstp_address_card.dart
@@ -10,69 +10,77 @@ class SstpAddressCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ListTile(
-        shape: const BeveledRectangleBorder(
-          side: BorderSide(
-            color: Colors.green,
-            width: 1.5,
-          ),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            bottomRight: Radius.circular(20.0),
-          ),
-        ),
-        leading: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              sstp.location?.short ?? "--",
-              style: const TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-              ),
+      child: Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(20),
+        shadowColor: Colors.black.withOpacity(0.1),
+        child: ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(
+              color: Colors.green,
+              width: 1.5,
             ),
-          ],
-        ),
-        subtitle: Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Row(
+          ),
+          leading: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Text(
-              //   sstp.location?.short ?? "--",
-              //   style: const TextStyle(color: Colors.green),
-              // ),
-              // const Text("  "),
-              Expanded(
-                child: Text(
-                  sstp.info ?? ("- " * 10),
-                  style: const TextStyle(fontSize: 11.0),
-                  // overflow: TextOverflow.ellipsis,
+              Text(
+                sstp.location?.short ?? "--",
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
               ),
             ],
           ),
-        ),
-        title: Text(
-          "${sstp.ip}:${sstp.port}",
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        trailing: Text(
-          "${sstp.ms ?? "---"}ms",
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        onTap: () {
-          Clipboard.setData(ClipboardData(text: "${sstp.ip}:${sstp.port}"));
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              duration: const Duration(milliseconds: 300),
-              content: Text("${sstp.ip}:${sstp.port} copied to clipboard"),
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              "${sstp.ip}:${sstp.port}",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
             ),
-          );
-        },
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    sstp.info ?? ("- " * 10),
+                    style: const TextStyle(fontSize: 11.0),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          trailing: Text(
+            "${sstp.ms ?? "---"}ms",
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+              color: Colors.green,
+            ),
+          ),
+          onTap: () {
+            Clipboard.setData(ClipboardData(text: "${sstp.ip}:${sstp.port}"));
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                duration: const Duration(milliseconds: 300),
+                content: Text(
+                  "${sstp.ip}:${sstp.port} скопировано в буфер обмена",
+                  style: const TextStyle(fontSize: 14),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
