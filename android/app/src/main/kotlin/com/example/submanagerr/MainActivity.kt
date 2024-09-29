507b760e054812e9379715030b838414c4d25513
ZhuZhuZhuang10 <157409639+ZhuZhuZhuang10@users.noreply.github.com>
2024-09-29 11:37:24 +0500

    Delete android directory

--- android/app/src/main/kotlin/com/example/submanagerr/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.submanagerr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/pinging/pinging/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pinging.pinging
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
