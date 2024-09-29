128282fea4a093df04b6c1a2b24b64e2e3cebd59
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-09-29 10:40:47 +0500

    Update app_error.dart

--- lib/data/error/app_error.dart
+++ lib/data/error/app_error.dart
@@ -4,36 +4,41 @@ import 'package:flutter/foundation.dart' show immutable;
 abstract class AppError {
   final String title;
   final String description;
+  final String? detailedError; // Новое поле для точной информации об ошибке
 
   const AppError({
     required this.title,
     required this.description,
+    this.detailedError, // Опциональное детализированное описание
   });
 }
 
 @immutable
 class LoadError extends AppError {
-  const LoadError()
+  const LoadError([String? detailedError])
       : super(
           title: "Load error",
           description: "Maybe internet connection error",
+          detailedError: detailedError, // Передаем детализированную ошибку
         );
 }
 
 @immutable
 class DioLoadError extends AppError {
-  const DioLoadError(String description)
+  const DioLoadError(String description, [String? detailedError])
       : super(
           title: "Load error",
           description: description,
+          detailedError: detailedError, // Передаем детализированную ошибку
         );
 }
 
 @immutable
 class DeviceIdAccessError extends AppError {
-  const DeviceIdAccessError()
+  const DeviceIdAccessError([String? detailedError])
       : super(
           title: "Access error",
           description: "Could not get device id",
+          detailedError: detailedError, // Передаем детализированную ошибку
         );
 }
