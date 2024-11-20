d872c7772c28fa1905ec67f59593f2f73e33c16c
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-11-20 13:37:40 +0500

    Fix MUX

--- lib/logic/utils/sstp_pinger.dart
+++ lib/logic/utils/sstp_pinger.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:pinging/data/models/sstp_data.dart';
-import 'package:pinging/data/models/sstp_data_result.dart';
 
 class SstpPinger {
-  final Duration? timeout;
+  final Duration timeout;
   final SstpDataModel sstp;
 
   SstpPinger(this.sstp, this.timeout);
@@ -29,137 +27,76 @@ class SstpPinger {
 }
 
 class BulkSstpPinger {
-  final Duration? timeout;
   final List<SstpDataModel> sstps;
-  final Function(SstpPingerResult sstpPinger)? onPing;
-  final Completer<void> cancelCompleter; // Для отмены
-  int doneCount = 0;
+  final Duration timeout;
+  final Function(SstpPingerResult)? onPing;
+  final Completer<void> cancelCompleter;
+  final int maxConcurrentPings;
 
   BulkSstpPinger({
     required this.sstps,
+    required this.timeout,
     this.onPing,
-    this.timeout,
     required this.cancelCompleter,
+    this.maxConcurrentPings = 5, // Ограничение параллельных задач
   });
 
   Future<List<SstpPingerResult>> pingAll() async {
-    // Параллельный запуск пингов для всех адресов
-    List<Future<SstpPingerResult>> pingers = [];
+    final results = <SstpPingerResult>[];
+    final sem = Semaphore(maxConcurrentPings);
 
     for (var sstp in sstps) {
-      // Проверка на отмену перед каждым пингом
-      if (cancelCompleter.isCompleted) {
-        break;
-      }
+      if (cancelCompleter.isCompleted) break;
 
-      pingers.add(SstpPinger(sstp, timeout).ping());
-    }
-
-    // Ожидаем завершения всех пингов параллельно
-    final results = await Future.wait(pingers);
-
-    for (var result in results) {
-      onPing?.call(result);
-      doneCount++;
+      await sem.acquire();
+      Future(() async {
+        final result = await SstpPinger(sstp, timeout).ping();
+        onPing?.call(result);
+        results.add(result);
+        sem.release();
+      });
     }
 
     return results;
   }
 }
 
-class BulkBulkSstpPinger {
-  final int count;
-  final Duration? timeout;
-  final Function(
-      SstpPingerResult sstpPinger,
-      ProgressStatus progress,
-      int index,
-      )? onPing;
-  final List<SstpDataModel> sstps;
-  final Completer<void> cancelCompleter; // Для отмены
+class Semaphore {
+  int _count;
+  final _queue = <Completer>[];
 
-  BulkBulkSstpPinger({
-    required this.count,
-    required this.sstps,
-    this.timeout = const Duration(milliseconds: 3000),
-    this.onPing,
-    required this.cancelCompleter,
-  });
+  Semaphore(this._count);
 
-  Future<List<SstpPingerResult>> start() async {
-    final chunks = sstps.splitIntoChunks(count);
-    final done = List<int>.generate(chunks.length, (_) => 0);
-
-    // Используем Future.wait для выполнения задач параллельно
-    List<Future<List<SstpPingerResult>>> futures = [];
-
-    for (int i = 0; i < chunks.length; i++) {
-      final sstpsChunk = chunks[i];
-
-      // Проверка на отмену перед запуском новой задачи
-      if (cancelCompleter.isCompleted) {
-        break;
-      }
-
-      futures.add(
-        BulkSstpPinger(
-          sstps: sstpsChunk,
-          timeout: timeout,
-          cancelCompleter: cancelCompleter, // Передаем Completer для отмены
-          onPing: (sstpPinger) {
-            onPing?.call(
-              sstpPinger,
-              ProgressStatus(++done[i], sstpsChunk.length),
-              i,
-            );
-          },
-        ).pingAll(),
-      );
+  Future<void> acquire() async {
+    if (_count > 0) {
+      _count--;
+    } else {
+      final completer = Completer<void>();
+      _queue.add(completer);
+      await completer.future;
     }
+  }
 
-    // Ожидаем завершения всех групп пингов
-    final list = await Future.wait(futures);
-
-    return list.joinChunks();
+  void release() {
+    if (_queue.isNotEmpty) {
+      _queue.removeAt(0).complete();
+    } else {
+      _count++;
+    }
   }
 }
 
-extension SplitIntoChunks<T> on List<T> {
-  List<List<T>> splitIntoChunks(int n) {
-    int chunkSize = (length / n).ceil();
-    List<List<T>> chunks = [];
+class SstpDataModel {
+  final String ip;
+  final int port;
 
-    for (var i = 0; i < length; i += chunkSize) {
-      chunks.add(sublist(
-        i,
-        i + chunkSize > length ? length : i + chunkSize,
-      ));
-    }
-
-    return chunks;
-  }
+  SstpDataModel(this.ip, this.port);
 }
 
-extension ChunksJoiner<T> on List<List<T>> {
-  List<T> joinChunks() {
-    List<T> result = [];
+class SstpPingerResult {
+  final SstpDataModel sstp;
+  final bool success;
+  final int ping;
 
-    for (var list in this) {
-      result.addAll(list);
-    }
-
-    return result;
-  }
-}
-
-class ProgressStatus {
-  final int count;
-  final int total;
-  final bool done;
-
-  ProgressStatus(this.count, this.total, [this.done = false]);
-
-  factory ProgressStatus.done() => ProgressStatus(0, 0, true);
-
-  double get value => total == 0 ? 0 : count / total;
+  SstpPingerResult(this.sstp, this.success, this.ping);
 }
